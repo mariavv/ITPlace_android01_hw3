98a5fe90a49aecf7226425a940798b2ba864628e
vasilevamariav <vasilevamariav@yandex.ru>
2017-08-18 21:14:38 +0400

    done

--- app/src/main/java/com/example/maria/itplace_android01_hw3/MainActivity.java
+++ app/src/main/java/com/example/maria/itplace_android01_hw3/MainActivity.java
@@ -16,7 +16,6 @@ import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telecom.CallScreeningService;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,25 +23,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URISyntaxException;
-
-import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MyTag";
     public static final String EXTRA_STRING = "extra_string";
     public static final String COLOR_KEY = "COLOR_KEY";
+    public static final String COLOR_KEY2 = "COLOR_KEY2";
     public static final String IMAGE_KEY = "IMAGE_KEY";
 
     public static final int REQUEST_CODE_HEX = 0;
     static final int GALLERY_REQUEST = 1;
 
+    TextView longTextView;
     Button button;
     TextView textView;
     ImageView imageView;
-    //String color;
-    Uri imageUri = null;
+    //Uri imageUri = null;
+    String img_path = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +54,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        Button btnSetColor1 = (Button) findViewById(R.id.btnSetColor1);
+        Button btnSetColor2 = (Button) findViewById(R.id.btnSetColor2);
+        Button btnSetColor3 = (Button) findViewById(R.id.btnSetColor3);
+
+        longTextView = (TextView) findViewById(R.id.textView);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int colorId = R.color.colorWhite;
+
+                switch (v.getId()) {
+                    case R.id.btnSetColor1:
+                        colorId = R.color.colorWhite;
+                        //textView.setBackgroundResource(R.color.colorWhite);
+                        break;
+                    case R.id.btnSetColor2:
+                        colorId = R.color.colorRed;
+                        //textView.setBackgroundResource(R.color.colorRed);
+                        break;
+                    case R.id.btnSetColor3:
+                        colorId = R.color.colorBlue;
+                        //textView.setBackgroundResource(R.color.colorBlue);
+                        break;
+                }
+
+                longTextView.setBackgroundResource(colorId);
+            }
+        };
+
+        btnSetColor1.setOnClickListener(onClickListener);
+        btnSetColor2.setOnClickListener(onClickListener);
+        btnSetColor3.setOnClickListener(onClickListener);
+
+
+
         button = (Button) findViewById(R.id.change_text_button);
         textView = (TextView) findViewById(R.id.our_text);
         imageView = (ImageView) findViewById(R.id.imageView);
@@ -84,22 +119,16 @@ public class MainActivity extends AppCompatActivity {
 
         textView.setText(savedInstanceState.getString(EXTRA_STRING));
 
-        //color = savedInstanceState.getString(COLOR_KEY);
-        //if (savedInstanceState.getInt(COLOR_KEY) != 0) {
-            textView.setBackgroundColor(savedInstanceState.getInt(COLOR_KEY)/*Color.parseColor(color)*/);
-        //}
+        textView.setBackgroundColor(savedInstanceState.getInt(COLOR_KEY));
+        longTextView.setBackgroundColor(savedInstanceState.getInt(COLOR_KEY2));
+
         String image_key = savedInstanceState.getString(IMAGE_KEY);
         if (image_key != null) {
-            imageUri = Uri.parse(image_key);
-            //Bitmap bitmap = null;
-            /*try {
-                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            imageView.setImageBitmap(bitmap);*/
+            /*imageUri = Uri.parse(image_key);
             imageView.setImageURI(null);  // для сброса кэша
-            imageView.setImageURI(imageUri);
+            imageView.setImageURI(imageUri); */
+            img_path = image_key;
+            setImage(image_key);
         }
     }
 
@@ -125,18 +154,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.d(TAG, "onSaveInstanceState: Called");
-        ColorDrawable colorDrawable = (ColorDrawable) textView.getBackground();
+        setColor(textView, COLOR_KEY, outState) ;
+        setColor(longTextView, COLOR_KEY2, outState) ;
+        /*ColorDrawable colorDrawable = (ColorDrawable) textView.getBackground();
         if (colorDrawable != null) {
-            outState.putInt(COLOR_KEY, /*Integer.toString(*/colorDrawable.getColor()/*)color*/);
+            outState.putInt(COLOR_KEY, colorDrawable.getColor());
         }
+        ColorDrawable colorDrawable2 = (ColorDrawable) longTextView.getBackground();
+        if (colorDrawable2 != null) {
+            outState.putInt(COLOR_KEY2, colorDrawable2.getColor());
+        }   */
         outState.putString(EXTRA_STRING, textView.getText().toString());
-        if (imageUri != null) {
+        /*if (imageUri != null) {
             outState.putString(IMAGE_KEY, imageUri.toString());
+        }  */
+        if (img_path != "") {
+            outState.putString(IMAGE_KEY, img_path);
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    private void setColor(TextView view, String key, Bundle outState) {
+        ColorDrawable colorDrawable = (ColorDrawable) view.getBackground();
+        if (colorDrawable != null) {
+            outState.putInt(key, colorDrawable.getColor());
+
+        }
+    }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode != RESULT_OK) {
@@ -144,105 +190,97 @@ public class MainActivity extends AppCompatActivity {
         }
         switch (requestCode) {
             case REQUEST_CODE_HEX:
-                String color = ResultActivity.getExtraString(data);
-                textView.setText(color);
-                textView.setBackgroundColor(Color.parseColor(color));
+                String str_color = ResultActivity.getExtraString(data);
+                textView.setText(str_color);
+                int color = Color.parseColor(str_color);
+                textView.setBackgroundColor(color);
+                longTextView.setBackgroundColor(color);
                 return;
             case GALLERY_REQUEST:
-                imageUri = data.getData();
+                Uri imageUri = data.getData();
 
-                /*Bitmap bitmap = null;
-                        try {
-                            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        imageView.setImageBitmap(bitmap); */
-
-                String simageUri = "";
                 try {
-                    simageUri = getFilePath(this, imageUri)  ;
+                    img_path = getFilePath(this, imageUri);
                 } catch (URISyntaxException e) {
                     e.printStackTrace();
                 }
-                    //imageUri  =  imageUri.parse(simageUri);
 
+                setImage(img_path);
 
-                File imgFile = new  File(simageUri);
-                if(imgFile.exists()){
-                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-                    imageView.setImageBitmap(myBitmap);
-                }
-
-                //imageView.setImageURI(null);  // для сброса кэша
-                //imageView.setImageURI(imageUri);
                 return;
         }
     }
 
-     @SuppressLint("NewApi")
-        public /*static*/ String getFilePath(Context context, Uri uri) throws URISyntaxException {
-            String selection = null;
-            String[] selectionArgs = null;
-            // Uri is different in versions after KITKAT (Android 4.4), we need to
-            if (Build.VERSION.SDK_INT >= 19 && DocumentsContract.isDocumentUri(context.getApplicationContext(), uri)) {
-                if (isExternalStorageDocument(uri)) {
-                    final String docId = DocumentsContract.getDocumentId(uri);
-                    final String[] split = docId.split(":");
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                } else if (isDownloadsDocument(uri)) {
-                    final String id = DocumentsContract.getDocumentId(uri);
-                    uri = ContentUris.withAppendedId(
-                            Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-                } else if (isMediaDocument(uri)) {
-                    final String docId = DocumentsContract.getDocumentId(uri);
-                    final String[] split = docId.split(":");
-                    final String type = split[0];
-                    if ("image".equals(type)) {
-                        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                    } else if ("video".equals(type)) {
-                        uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                    } else if ("audio".equals(type)) {
-                        uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                    }
-                    selection = "_id=?";
-                    selectionArgs = new String[]{
-                            split[1]
-                    };
+    private void setImage(String image_path) {
+                File imgFile = new File(image_path);
+                if (imgFile.exists()) {
+                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                    imageView.setImageBitmap(myBitmap);
                 }
-            }
-            if ("content".equalsIgnoreCase(uri.getScheme())) {
-                String[] projection = {
-                        MediaStore.Images.Media.DATA
+    }
+
+    @SuppressLint("NewApi")
+    public /*static*/ String getFilePath(Context context, Uri uri) throws URISyntaxException {
+        String selection = null;
+        String[] selectionArgs = null;
+        // Uri is different in versions after KITKAT (Android 4.4), we need to
+        if (Build.VERSION.SDK_INT >= 19 && DocumentsContract.isDocumentUri(context.getApplicationContext(), uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                uri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("image".equals(type)) {
+                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                selection = "_id=?";
+                selectionArgs = new String[]{
+                        split[1]
                 };
-                Cursor cursor = null;
-                try {
-                    cursor = context.getContentResolver()
-                            .query(uri, projection, selection, selectionArgs, null);
-                    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                    if (cursor.moveToFirst()) {
-                        return cursor.getString(column_index);
-                    }
-                } catch (Exception e) {
-                }
-            } else if ("file".equalsIgnoreCase(uri.getScheme())) {
-                return uri.getPath();
             }
-            return null;
         }
-
-        public static boolean isExternalStorageDocument(Uri uri) {
-            return "com.android.externalstorage.documents".equals(uri.getAuthority());
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = {
+                    MediaStore.Images.Media.DATA
+            };
+            Cursor cursor = null;
+            try {
+                cursor = context.getContentResolver()
+                        .query(uri, projection, selection, selectionArgs, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
         }
+        return null;
+    }
 
-        public static boolean isDownloadsDocument(Uri uri) {
-            return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-        }
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
 
-        public static boolean isMediaDocument(Uri uri) {
-            return "com.android.providers.media.documents".equals(uri.getAuthority());
-        }
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
 
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
 
 
 }
